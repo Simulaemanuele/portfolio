a1766f348ff490bd3900d0be7d52365324f3e682
Emanuele Simula <simulaemanuele@gmail.com>
2024-08-13 12:58:07 +0200

    feat: created Button.tsx DropdownMenu.tsx and Toggle.tsx custom component and install a bunch of node modules packages and temporarily implemented ModeToggle in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
+import { ModeToggle } from "@/components/ui/Toggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full ">
+        <ModeToggle />
         <Hero />
       </div>
     </main>
