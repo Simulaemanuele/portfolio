c3772b5d46a9f2b11737f5949e95832fdf3fb04d
Emanuele Simula <simulaemanuele@gmail.com>
2024-08-13 13:53:00 +0200

    feat: created and implemented MagicButton.tsx in Hero section, updated TextGenerateEffect.tsx custom component and installed react-icons package. Hero section completed!

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa6";
 
 const Hero = () => {
   return (
@@ -30,6 +32,14 @@ const Hero = () => {
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m Emanuele, a Fullstack developer based in Italy.
           </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
         </div>
       </div>
     </div>
