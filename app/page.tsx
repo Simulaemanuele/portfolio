c7e68cbb6be8b9f931e6d7044e010573c8940035
Emanuele Simula <simulaemanuele@gmail.com>
2024-08-22 13:31:34 +0200

    feat: first created Experience.tsx custom component section and successfully implemented in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -13,12 +14,12 @@ export default function Home() {
   return (
     <main className="relative dark:bg-black-100 bg-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full ">
-        {/* <ModeToggle /> */}
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience />
       </div>
     </main>
   );
