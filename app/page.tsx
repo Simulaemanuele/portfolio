16212e4923cb2cff0edf12a695c4ba88c66eb7bc
Emanuele Simula <simulaemanuele@gmail.com>
2024-09-05 17:30:35 +0200

    fix: fix in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { appWithTranslation } from "next-i18next";
 import "../i18n";
 import dynamic from "next/dynamic";
 
 const Main = dynamic(() => import("@/components/Main"));
 
-function Home() {
+export default function Home() {
   return <Main />;
 }
-
-export default appWithTranslation(Home);
