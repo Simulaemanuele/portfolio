71e56b51b65cac9641840639e75840db1cb1dc2e
Simulaemanuele <simulaemanuele@gmail.com>
2026-03-16 21:45:36 +0100

    update: implemented Clarity tracking script to track heat maps.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,15 @@ function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vwu0ago999");
+          `}
+        </Script>
       </body>
     </html>
   );
